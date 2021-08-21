831fc0e67518b32596c8a274aac042f32fdd4f7f
Devaka Dabare <devakadabare01@gmail.com>
2021-08-21 11:14:35 +0530

    first unit test case

--- test/orders.js
+++ test/orders.js
@@ -10,30 +10,14 @@ const data = {
     items : [{itemId:1 ,quantity: 1, notes: ''},{itemId:2 ,quantity: 1, notes: ''}]
 }
 
-// describe('Add Order', function() {
-//   describe('#indexOf()', function() {
-//     it('should return -1 when the value is not present', function() {
-//       assert.equal([1, 2, 3].indexOf(4), -1);
-//     });
-//   });
-// });
-
 describe('Add Order', async function() {
-    describe('#indexOf()', function() {
-        it("returns status 200", function(done) {
-            // request.post(url,data, function(error, response, body) {
-            //     expect(response.statusCode).to.equal(201);
-            //     done();
-            //   });
-            // request.post(url,data, function(error, response, body) {
-            //     expect(response.statusCode).to.equal(200);
-            //     done();
-            //   }))
-            ordersService.addOrder(data, function({statusCode, body}) {
-                expect(statusCode).to.equal(201);
-                 done();
-            });
+    
+    it("returns status 201", function(done) {
+        ordersService.addOrder(data, function({statusCode, body}) {
+            expect(statusCode).to.equal(201);
+                done();
         });
     });
+    
 });
 
