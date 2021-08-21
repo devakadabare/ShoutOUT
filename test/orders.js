dae64267e22d9c13efdb98f1a38717bdfe2783c8
Devaka Dabare <devakadabare01@gmail.com>
2021-08-21 10:05:25 +0530

    intilize mocha

--- test/orders.js
+++ test/orders.js
@@ -1,5 +1,7 @@
 var assert = require('assert');
-describe('Array', function() {
+const ordersService = require('../src/api/services/orders');
+
+describe('Add Order', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
       assert.equal([1, 2, 3].indexOf(4), -1);
